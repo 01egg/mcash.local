68c65c67d5643c603a82ba01c73037f06e9c0df2
Григоришин Олег Игоревич <D:\MAIL>
2020-02-20 17:29:57 +0300

    partners, faq, review

---
+++ js/script.js
@@ -0,0 +1,26 @@
+(function ($) {
+    $(document).ready(function () {
+
+        // OWL CARUSEL
+        $('.review_carusel').owlCarousel({
+            items:2,
+            stagePadding: 350,
+            loop:true,
+            margin:50,
+            nav:false,
+            autoplay:true,
+        });
+
+        // BUTTON HIDE
+        $('.faq_hid-btn').click(function () {
+            $('.faq_hid-left').appendTo('.faq_last-left').hide().fadeIn(2000);
+            $('.faq_hid-btn').fadeOut();
+        }); 
+
+        $('.faq_hid-btn').click(function () {
+            $('.faq_hid-right').appendTo('.faq_last-right').hide().fadeIn(2000);
+            $('.faq_hid-btn').fadeOut();
+        }); 
+
+    });
+    })(jQuery);
