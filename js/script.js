3249130467901eb29a99cc9c03069d8f141f1d5d
Григоришин Олег Игоревич <D:\MAIL>
2020-02-21 16:03:54 +0300

    map

--- js/script.js
+++ js/script.js
@@ -22,5 +22,12 @@
             $('.faq_hid-btn').fadeOut();
         }); 
 
+        // MAP CLICK
+        $('.map').click(function(){
+            $(this).find('iframe').addClass('clicked')
+        }).mouseleave(function(){
+            $(this).find('iframe').removeClass('clicked')
+        });
+
     });
     })(jQuery);
